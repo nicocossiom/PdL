a283eb52fd33431eed3b66cf813ce24912f73a16
NicolasCossio <nicocossiom@gmail.com>
2021-10-22 13:55:11 +0200

    Terminando de reconocer caracteres falta terminar genToken de ids y de palabras reservadas
    Co-authored-by: barbararodriguezr <barbararodriguezr@users.noreply.github.com>
    Co-authored-by: infern <infern7@users.noreply.github.com>

--- lexer.py
+++ lexer.py
@@ -2,23 +2,38 @@
 #!/usr/bin/python3
 #!/usr/bin/pyton3.10
 import sys
-from types import CodeType
 MIN_PYTHON = (3, 10)
 if sys.version_info < MIN_PYTHON:
     sys.exit("Python %s.%s or later is required.\n" % MIN_PYTHON)
 
 if len(sys.argv) > 2: sys.exit("Input file path to analyze as program argumen")
 
+
 #file that the lexer will generate tokens for 
 f = open(sys.argv[1], "r")
 #Lenguage definitions by class
 DELIMITERS = " \n\t"
-DIGITS = "123456789"
-LETTERS = "abcdefghijlmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
-SYMS_OPS = ["+","*","&","=",">", "," ,";", "(",")","{","}", ""]
-TOKEN_CODES = ["mas", "por", "and", "asig", "mayor", "coma", "puntoComa", "parAbierto", "parCerrado", "llaveAbierto", "llaveAbierto", "eof"]
 RES_WORD = [ "let","function","rn","else","input","print","while","do","true","false","int","boolean","string"]
+LETTERS = "abcdefghijlmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+SYMB_OPS ={
+    "+": "mas",
+    "*": "por",
+    "&": "and",
+    "=": "asig",
+    ">": "mayor",
+    ",": "coma",
+    ";": "puntoComa",
+    "(": "parAbierto",
+    ")": "parCerrado",
+    "{": "llaveAbierto",
+    "}": "llaveCerrado"
+}
 
+def peek_nextCar(f):
+    pos = f.tell()
+    f.seek(pos)
+    car = f.read(1)
+    return car
 class Token:
     def __init__(self, type:str, attribute):
         self.code = type
@@ -28,9 +43,9 @@ class Lexer:
     def __init__(self):
         self.num = 0 #current integer number being constructed
         self.lex = "" #current string being constructed
-        self.car = self.read(1) #current character being read
+        self.car = f.read(1) #current character being read
 
-    def next(self) -> str :
+    def next(self):
         '''Advances current character to the next one from the file'''
         self.car = f.read(1)
 
@@ -47,32 +62,57 @@ class Lexer:
         while self.car in DELIMITERS: 
             self.next()
 
-    def genToken(self, code:str, attribute)
+    def genToken(self, code:str, attribute) -> Token: 
+        token = Token(code, attribute)
+        return token
+
     def tokenize(self):
         while self.next() is None and self.car != "":
             #Integer being formed 
-            if self.car in DIGITS and self.lex=="":
+            if (self.car).isDigit() and self.lex=="":
                 self.generateNumber()
-            #Lexeme being formed 
-            elif self.lex != "": self.concatenate()
+                if(not peek_nextCar(f).isDigit()): self.genToken("cteEntera", self.num)
+            #Identifiers or Reserved Words
+            elif self.car in LETTERS and self.lex != "": 
+                self.concatenate()
+                nextCar = peek_nextCar(f)
+                if( nextCar not in LETTERS):
+                    if self.lex in RES_WORD: self.genToken(id,self.lex)
+                    else: self.genToken(self.lex, None)
             #Block comment processing
             elif self.car == "/" and self.next() is None and self.car == "*": 
                 while self.car != "*" and self.next() is None and self.car == "/":
-                    pass
+                    self.next()
+            #String (cadena) processing
+            elif self.car == "\"": 
+                while self.car != "\"":
+                    self.concatenate()
+                    self.next()
+                self.genToken("cadena",self.lex)
             #Operators, symbols
-            elif self.car in SYMS_OPS:
-                index = SYMS_OPS.index(self.car)
-                match(index):
-                    case 0: 
-
-                    case 1:
-
-                    case 2:
-
-                    case 3:
-
-                    case 4:
-
-
-self.close()
-             
+            elif self.car in SYMB_OPS:
+                #+ or ++
+                if(self.car == "+"):
+                    if (peek_nextCar(f) == "+") :
+                        self.genToken("postIncrem", None)
+                        self.next()
+                    else: self.genToken("mas", None)
+                #&&
+                elif (self.car == "&"):
+                    if (peek_nextCar(f) == "&") : 
+                        self.genToken("and", None)
+                        self.next()
+                #= or ==
+                elif(self.car == "="):
+                    if (peek_nextCar(f) == "="):
+                        self.genToken("equals", None)
+                        self.next()
+                    else:
+                        self.genToken("asig", None)
+                else:
+                    self.genToken(SYMB_OPS[self.car], None)
+                        
+               
+                #Identitificadores
+                
+ 

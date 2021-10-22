64812f80ec817290ee20ce0925f95e64fd0023b9
NicoCossio <nicocossiom@gmail.com>
2021-10-22 22:16:02 +0200

    Lexer finalizado falta tabla de simbolos
    Co-authored-by: barbararodriguezr <barbararodriguezr@users.noreply.github.com>
    Co-authored-by: infern <infern7@users.noreply.github.com>

--- lexer.py
+++ lexer.py
@@ -2,17 +2,11 @@
 #!/usr/bin/python3
 #!/usr/bin/pyton3.10
 import sys
-MIN_PYTHON = (3, 10)
-if sys.version_info < MIN_PYTHON:
-    sys.exit("Python %s.%s or later is required.\n" % MIN_PYTHON)
 
-if len(sys.argv) > 2: sys.exit("Input file path to analyze as program argumen")
-
-
+if len(sys.argv) > 2: sys.exit("Input file path to analyze as program argument")
 #file that the lexer will generate tokens for 
 f = open(sys.argv[1], "r")
 #Lenguage definitions by class
-DELIMITERS = " \n\t"
 RES_WORD = [ "let","function","rn","else","input","print","while","do","true","false","int","boolean","string"]
 LETTERS = "abcdefghijlmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
 SYMB_OPS ={
@@ -28,12 +22,14 @@ SYMB_OPS ={
     "{": "llaveAbierto",
     "}": "llaveCerrado"
 }
+ERROR_CODE = {0:"Comentario mal puesto"}
 
 def peek_nextCar(f):
     pos = f.tell()
-    f.seek(pos)
     car = f.read(1)
+    f.seek(pos)
     return car
+
 class Token:
     def __init__(self, type:str, attribute):
         self.code = type
@@ -44,11 +40,15 @@ class Lexer:
         self.num = 0 #current integer number being constructed
         self.lex = "" #current string being constructed
         self.car = f.read(1) #current character being read
+        self.tokenList = [] 
 
     def next(self):
         '''Advances current character to the next one from the file'''
         self.car = f.read(1)
-
+        if self.car != "": 
+            while(ord(self.car) < 33):
+                self.car = f.read(1)
+        
     def generateNumber(self):
         '''Adds current char to number in construction after multiplying the number by 10 '''
         self.num = self.num * 10 + int(self.car)        
@@ -57,34 +57,50 @@ class Lexer:
         '''Concatenates current char to lexeme in contruction'''
         self.lex = self.lex + self.car
 
-    def skipDelimeters(self):
-        '''Skips white spaces, new lines and tabs'''
-        while self.car in DELIMITERS: 
-            self.next()
+    def printToken(self,):
+        with open("token.txt", "w") as f:
+            for token in self.tokenList:
+                f.write(f"< {token.code} , {token.att} >\n".replace("None", "-"))
+    
+    
 
+# < codigo , atributo > 
     def genToken(self, code:str, attribute) -> Token: 
         token = Token(code, attribute)
+        self.tokenList.append(token)
+        self.lex = ""
+        self.num = 0 
         return token
 
+    def error(self, tipo: int):
+        sys.exit("ERROR:", ERROR_CODE[tipo])
+
     def tokenize(self):
-        while self.next() is None and self.car != "":
+        while self.car != "":
             #Integer being formed 
-            if (self.car).isDigit() and self.lex=="":
+            if (self.car).isdigit() and self.lex=="":
                 self.generateNumber()
-                if(not peek_nextCar(f).isDigit()): self.genToken("cteEntera", self.num)
+                if(not peek_nextCar(f).isdigit()): self.genToken("cteEntera", self.num)
             #Identifiers or Reserved Words
-            elif self.car in LETTERS and self.lex != "": 
+            elif self.car in LETTERS or self.lex != "":
                 self.concatenate()
                 nextCar = peek_nextCar(f)
-                if( nextCar not in LETTERS):
-                    if self.lex in RES_WORD: self.genToken(id,self.lex)
+                if(nextCar not in LETTERS or nextCar == "_" or nextCar.isdigit() or nextCar ==""):
+                    if self.lex in RES_WORD: self.genToken("id",self.lex)
                     else: self.genToken(self.lex, None)
             #Block comment processing
-            elif self.car == "/" and self.next() is None and self.car == "*": 
-                while self.car != "*" and self.next() is None and self.car == "/":
+            elif self.car == "/": 
+                self.next()
+                if self.car == "*":
                     self.next()
+                    while (peek_nextCar(f) != "/" and self.car != "*"):
+                        self.next()
+                        self.next()
+                else: self.error(0)
+                self.next()
             #String (cadena) processing
             elif self.car == "\"": 
+                self.next()
                 while self.car != "\"":
                     self.concatenate()
                     self.next()
@@ -111,8 +127,6 @@ class Lexer:
                         self.genToken("asig", None)
                 else:
                     self.genToken(SYMB_OPS[self.car], None)
-                        
-               
-                #Identitificadores
-                
- 
+            self.next()
+
+    
--- pruebas.py
+++ pruebas.py
@@ -1,28 +1,4 @@
-import sys
-
-# l = Lexer("input")
-# l.correrLines()
-
-# class prueba:
-#     def __init__(self):
-#         self.a="/"
-#         self.f = open("input", "r")
-#         self.a = self.f.read(1) 
-
-#     def cambiar(self):
-#         self.a = "*"
-#         print("cambiado")
-
-#     def next(self):
-#         self.a = self.f.read(1) 
-# p = prueba()
-
-# if p.a == "/" and p.cambiar() is None and p.a == "*":
-#     print(p.a)
-
-# while p.a != "":
-#     p.next()
-#     i=i+1
-#     print(i)
-
-print(len(sys.argv))
+from lexer import Lexer
+lexer = Lexer()
+lexer.tokenize()
+lexer.printToken()
